0a6f79569c119dd759d60663c0a8b487b891d17a
mmaddina <mamedovamadinaa8@gmail.com>
2023-01-02 17:08:55 +0300

    Task 50 done

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ void PrintArray(double[,] array)
 //5 9 2 3
 //8 4 2 4
 //17 -> такого числа в массиве нет
-
+/*
 Console.WriteLine("Введите размеры массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int n = Convert.ToInt32(Console.ReadLine());
@@ -73,7 +73,7 @@ for (int i = 0; i < array.GetLength(0); i++)
  object c = array.GetValue(a,b);
  Console.WriteLine(c);
  }
-
+*/
 
 
 //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
@@ -82,3 +82,28 @@ for (int i = 0; i < array.GetLength(0); i++)
 //5 9 2 3
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
